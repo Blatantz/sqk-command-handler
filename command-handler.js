0835e111f82faf9fb67d67d5bf9f4c4c94a6f6f4
Sqaaakoi <sqeeeky20@gmail.com>
2019-01-17 13:56:38 +1300

    Fixing stuff so now it actually works yAy
    oh hello why are you reading this theres no point of reading this

--- command-handler.js
+++ command-handler.js
@@ -20,7 +20,7 @@ module.exports = (command, prefix, discordMentionID) => {
     return response;
   }
 
-  if (prefix == undefined && prefix != null) {
+  if (prefix === undefined && prefix !== null) {
     throw new ReferenceError("\"prefix\" is undefined");
     let response = {
       command: null,
@@ -30,7 +30,7 @@ module.exports = (command, prefix, discordMentionID) => {
     }
     return response;
   }
-  if (typeof prefix != "string" && (discordMentionID && prefix != null)) {
+  if (typeof prefix != "string" && (discordMentionID && prefix !== null)) {
     throw new TypeError("\"prefix\" is not a string and isn't disabled");
     let response = {
       command: null,
@@ -47,7 +47,7 @@ module.exports = (command, prefix, discordMentionID) => {
     }
   }
 
-  if (prefix) {
+  if (typeof prefix == "string") {
     // Checks if message starts with the prefix.
     let prfch = command.split("").slice(0, prefix.length).join("");
     if (prfch == prefix) {
@@ -66,6 +66,60 @@ module.exports = (command, prefix, discordMentionID) => {
         args: subcommand,
         text: saytext,
         success: true
+      }
+      return response;
+    } else if (discordMentionID) {
+      let prfstrm = "<@" + discordMentionID + "> ";
+      let prfch = command.split("").slice(0, prfstrm.length).join("");
+      if (prfch == prfstrm) {
+        // This is so prefixes with spaces don"t break everything.
+        let prfspc = prfch.split(" ").length - 1;
+        // Fetches the command and prefix.
+        let first = command.split(" ").slice(0, 1 + prfspc).join(" ");
+        // Sets the command. Use if (command == "command") to test for commands.
+        let cmd = command.split("").slice(prfch.length, first.length).join("");
+        // Arguments as a array.
+        let subcommand = command.split(" ").slice(1 + prfspc);
+        // Quick way to get args as string
+        let saytext = subcommand.join(" ");
+        // console.log(prfch + ":" + first + ":" + command + ":" + subcommand.join(" "));
+        let response = {
+          command: cmd,
+          args: subcommand,
+          text: saytext,
+          success: true
+        }
+        return response;
+      }
+
+      prfstrm = "<@!" + discordMentionID + "> ";
+      prfch = command.split("").slice(0, prfstrm.length).join("");
+      if (prfch == prfstrm) {
+        // This is so prefixes with spaces don"t break everything.
+        let prfspc = prfch.split(" ").length - 1;
+        // Fetches the command and prefix.
+        let first = command.split(" ").slice(0, 1 + prfspc).join(" ");
+        // Sets the command. Use if (command == "command") to test for commands.
+        let cmd = command.split("").slice(prfch.length, first.length).join("");
+        // Arguments as a array.
+        let subcommand = command.split(" ").slice(1 + prfspc);
+        // Quick way to get args as string
+        let saytext = subcommand.join(" ");
+        // Return
+        let response = {
+          command: cmd,
+          args: subcommand,
+          text: saytext,
+          success: true
+        }
+        return response;
+      }
+    } else {
+      let response = {
+        command: null,
+        args: null,
+        text: null,
+        success: false
       }
       return response;
     }
@@ -115,12 +169,13 @@ module.exports = (command, prefix, discordMentionID) => {
       }
       return response;
     }
+  } else {
+    let response = {
+      command: null,
+      args: null,
+      text: null,
+      success: false
+    }
+    return response;
   }
-  let response = {
-    command: null,
-    args: null,
-    text: null,
-    success: false
-  }
-  return response;
 }
